d8ce0d8e67b2196ae9add733b2ff3c7944ace8ed
Paulo Henrique <pauloheni@hotmail.com>
2022-09-24 16:40:16 -0300

    Exercicio5

--- src/Exercicio_Easy/Exerc5.java
+++ src/Exercicio_Easy/Exerc5.java
@@ -3,35 +3,64 @@ package Exercicio_Easy;
 import javax.swing.*;
 
 public class Exerc5 {
-    public static void main(String[] args) {
-//        ▶ Exercício 5 - Faça um algoritmo para ler três números inteiros, armazenar em variáveis, e exibir
-//        a soma, a subtração, multiplicação e a média dos três números digitados. - PÁGINA 16
-//        ----Inicio Algoritmo
-//        numero num1, num2, num3, result;
-//        num1 = Ler();
-//        num2 = Ler();
-//        num3 = Ler();
-//        result = num+num+num3
-//        Exibir: "A soma dos números é: " + result;
-//        result = num-num2-num3
-//        Exibir: "A subtração dos números é: " + result;
-//        result = num1*num2*num3
-//        Exibir: "A mutiplicação dos números é: " + num1*num2*num3;
-//        Exibir: "A média dos números é: " + (num1+num2+num3)/3;
-//        ----Fim Algoritmo
 
-        int num1, num2, num3, result;
-        num1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número"));
-        num2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número"));
-        num3 = Integer.parseInt(JOptionPane.showInputDialog("Digite o terceiro número"));
-        result = num1 + num2 + num3;
-        System.out.println("A soma dos números é de: " + result);
-        result = num1 - num2 - num3;
-        System.out.println("A subtração dos números é de: " + result);
-        result = num1 * num2 * num3;
-        System.out.println("A multiplicação dos números é de: " + result);
-        System.out.println("A média dos números é de: " + (num1 + num2 + num3)/3);
 
+    public int soma(int num1, int num2, int num3) {
+        return num1 + num2 + num3;}
+    public int sub(int num1, int num2, int num3) {
+        return num1 - num2 - num3;}
+    public int mult(int num1, int num2, int num3) {
+        return num1 * num2 * num3;}
+    public double divi(double num1, double num2, double num3) {
+        return num1 / num2 / num3;}
+
+
+
+//        System.out.println("A soma dos números é de: " + result);
+//        result = num1 - num2 - num3;
+//        System.out.println("A subtração dos números é de: " + result);
+//        result = num1 * num2 * num3;
+//        System.out.println("A multiplicação dos números é de: " + result);
+//        System.out.println("A média dos números é de: " + (num1 + num2 + num3) / 3);
     }
 
-}
+
+
+
+
+
+
+
+
+//    public static void main(String[] args) {
+////        ▶ Exercício 5 - Faça um algoritmo para ler três números inteiros, armazenar em variáveis, e exibir
+////        a soma, a subtração, multiplicação e a média dos três números digitados. - PÁGINA 16
+////        ----Inicio Algoritmo
+////        numero num1, num2, num3, result;
+////        num1 = Ler();
+////        num2 = Ler();
+////        num3 = Ler();
+////        result = num+num+num3
+////        Exibir: "A soma dos números é: " + result;
+////        result = num-num2-num3
+////        Exibir: "A subtração dos números é: " + result;
+////        result = num1*num2*num3
+////        Exibir: "A mutiplicação dos números é: " + num1*num2*num3;
+////        Exibir: "A média dos números é: " + (num1+num2+num3)/3;
+////        ----Fim Algoritmo
+//
+//        int num1, num2, num3, result;
+//        num1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número"));
+//        num2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número"));
+//        num3 = Integer.parseInt(JOptionPane.showInputDialog("Digite o terceiro número"));
+//        result = num1 + num2 + num3;
+//        System.out.println("A soma dos números é de: " + result);
+//        result = num1 - num2 - num3;
+//        System.out.println("A subtração dos números é de: " + result);
+//        result = num1 * num2 * num3;
+//        System.out.println("A multiplicação dos números é de: " + result);
+//        System.out.println("A média dos números é de: " + (num1 + num2 + num3)/3);
+//
+//    }
+
+
--- src/exemplos/Exercicos_2.java
+++ src/exemplos/Exercicos_2.java
@@ -3,6 +3,7 @@ package exemplos;
 import Exercicio_Easy.Exer2;
 import Exercicio_Easy.Exer3;
 import Exercicio_Easy.Exer4;
+import Exercicio_Easy.Exerc5;
 
 import java.sql.SQLOutput;
 
@@ -22,9 +23,19 @@ public class Exercicos_2 {
 //        System.out.println(exer3.dificil("Paulo"," Henrique"));
 //    }
 
+    ////    //EXERC 4
+//    public static void main(String[] args) {
+//        Exer4 exer4 = new Exer4();
+//        System.out.println("O dobro é :" + exer4.vezesdobro(5) + " é o dobro");
+//    }
+//}
     public static void main(String[] args) {
-        Exer4 exer4 = new Exer4();
-        System.out.println(exer4.vezesdobro(5));
-    }
+        Exerc5 exerc5 = new Exerc5();
+        int var1 = 1, var2 = 2, var3 = 3;
 
+        System.out.println(exerc5.soma(var1, var2, var3));;
+        System.out.println(exerc5.sub(var1,var2,var3));
+        System.out.println(exerc5.mult(var1,var2,var3));
+        System.out.println(exerc5.divi(var1,var2,var3));
+    }
 }

eec7b696e32f95bdcb7346433959dd4dc006866c
Paulo Henrique <pauloheni@hotmail.com>
2022-09-24 17:03:45 -0300

    Atualizado 6

--- src/ExercicoMedio/ExerMedio1.java
+++ src/ExercicoMedio/ExerMedio1.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.sql.SQLOutput;
 
 public class ExerMedio1 {
-    public static void main(String[] args) {
+    public void exibirParImpar() {
 
         //    Exercício 1 - Faça um algoritmo para verificar e exibir de 0 a 100,
 //    quais são os números Pares e ímpares. - PÁGINA 23
@@ -18,14 +18,14 @@ public class ExerMedio1 {
 //            System.out.println("Impar");
 //        }
 
-        int i = 0;
-        while (i<=100) {
-            if(i%2 ==0) {
-                System.out.println("O número é: " + i + " é par");
+        int num=0;
+        while (num<=100) {
+            if(num%2 ==0) {
+                System.out.println("O número é: " + num + " é par");
             }else{
-                System.out.println("O numero é: " + i + " é impar");
+                System.out.println("O numero é: " + num + " é impar");
             }
-            i++;
+            num++;
         }
 
 
--- src/exemplos/Exercicos_2.java
+++ src/exemplos/Exercicos_2.java
@@ -4,6 +4,7 @@ import Exercicio_Easy.Exer2;
 import Exercicio_Easy.Exer3;
 import Exercicio_Easy.Exer4;
 import Exercicio_Easy.Exerc5;
+import ExercicoMedio.ExerMedio1;
 
 import java.sql.SQLOutput;
 
@@ -17,25 +18,38 @@ public class Exercicos_2 {
 //    }
 //}
 
-//    //EXERC 3
+//    //EXERC 3 easy
 //    public static void main(String[] args) {
 //        Exer3 exer3 = new Exer3();
 //        System.out.println(exer3.dificil("Paulo"," Henrique"));
 //    }
 
-    ////    //EXERC 4
+    ////    //EXERC 4 easy
 //    public static void main(String[] args) {
 //        Exer4 exer4 = new Exer4();
 //        System.out.println("O dobro é :" + exer4.vezesdobro(5) + " é o dobro");
 //    }
 //}
-    public static void main(String[] args) {
-        Exerc5 exerc5 = new Exerc5();
-        int var1 = 1, var2 = 2, var3 = 3;
 
-        System.out.println(exerc5.soma(var1, var2, var3));;
-        System.out.println(exerc5.sub(var1,var2,var3));
-        System.out.println(exerc5.mult(var1,var2,var3));
-        System.out.println(exerc5.divi(var1,var2,var3));
-    }
+
+//    EXERC 5 easy
+//    public static void main(String[] args) {
+//        Exerc5 exerc5 = new Exerc5();
+//        int var1 = 8, var2 = 10, var3 = 2;
+//
+//        System.out.println(exerc5.soma(var1, var2, var3));;
+//        System.out.println(exerc5.sub(var1,var2,var3));
+//        System.out.println(exerc5.mult(var1,var2,var3));
+//        System.out.println(exerc5.divi(var1,var2,var3));
+//    }
+
+    //EXERC 6 easy
+        public static void main(String[] args) {
+            ExerMedio1 exerMedio1 = new ExerMedio1();
+            System.out.println(exerMedio1.exibirParImpar(););
+
+
+
+}
+
 }
